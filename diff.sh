353204a64fc188d43df8ea19584fb253d262a61d
Toru Furukawa <torufurukawa@gmail.com>
2016-11-24 16:07:23 +0900

    Add diff wrapper as shell script

---
+++ diff.sh
@@ -0,0 +1,14 @@
+# Clean remote file path
+REMOTE_FILENAME=$1
+REMOTE_PATH=$(pwd)/./${REMOTE_FILENAME}
+REMOTE_WINPATH=$(cygpath -w ${REMOTE_PATH})
+echo ${REMOTE_WINPATH}
+
+# Clean local file path
+LOCAL_PATH=$2
+LOCAL_WINPATH=$(cygpath -w ${LOCAL_PATH})
+echo ${LOCAL_WINPATH}
+
+# Run LabVIEW Compare
+# $ "C:\Program Files (x86)\National Instruments\Shared\LabVIEW Compare\LVCompare.exe" "C:\Users\Toru Furukawa\Desktop\gitlv2\foo.vi" "C:\Users\TORUFU~1\AppData\Local\Temp\abc_foo.vi"
+"C:\Program Files (x86)\National Instruments\Shared\LabVIEW Compare\LVCompare.exe" "${REMOTE_WINPATH}" "${LOCAL_WINPATH}"
