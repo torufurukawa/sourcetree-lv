c5a4c616e23525d4a1ca9db93d6a500d28a9eae1
Toru Furukawa <torufurukawa@gmail.com>
2016-11-25 10:34:14 +0900

    Refactor

--- diff.sh
+++ diff.sh
@@ -1,7 +1,6 @@
 # Clean remote file path
 REMOTE_FILENAME=$1
 REMOTE_PATH=$(pwd)/./${REMOTE_FILENAME}
-echo ${REMOTE_PATH}
 REMOTE_WINPATH=$(cygpath -d "${REMOTE_PATH}")
 echo ${REMOTE_WINPATH}
 
@@ -11,5 +10,5 @@ LOCAL_WINPATH=$(cygpath -d "${LOCAL_PATH}")
 echo ${LOCAL_WINPATH}
 
 # Run LabVIEW Compare
-# $ "C:\Program Files (x86)\National Instruments\Shared\LabVIEW Compare\LVCompare.exe" "C:\Users\Toru Furukawa\Desktop\gitlv2\foo.vi" "C:\Users\TORUFU~1\AppData\Local\Temp\abc_foo.vi"
-"C:\Program Files (x86)\National Instruments\Shared\LabVIEW Compare\LVCompare.exe" "${REMOTE_WINPATH}" "${LOCAL_WINPATH}"
+DIFF="C:\Program Files (x86)\National Instruments\Shared\LabVIEW Compare\LVCompare.exe"
+"${DIFF}" "${REMOTE_WINPATH}" "${LOCAL_WINPATH}"
