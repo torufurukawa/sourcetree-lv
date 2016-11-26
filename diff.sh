52b9c294b608d87389738bf1b60cf812183c5261
Toru Furukawa <torufurukawa@gmail.com>
2016-11-26 18:57:08 +0900

    Combile resolving absolut path and DOS path

--- diff.sh
+++ diff.sh
@@ -3,13 +3,12 @@ source "${SCRIPT_DIR}/config.txt"
 source "${SCRIPT_DIR}/utils.sh"
 
 # Clean remote file path
-REMOTE_FILENAME=$1
-REMOTE_PATH=$(pwd)/./${REMOTE_FILENAME}
-REMOTE_WINPATH=$(dospath "${REMOTE_PATH}")
+REMOTE_PATH=$1
+REMOTE_WINPATH=$(resolve_path "${REMOTE_PATH}")
 
 # Clean local file path
 LOCAL_PATH=$2
-LOCAL_WINPATH=$(dospath "${LOCAL_PATH}")
+LOCAL_WINPATH=$(resolve_path "${LOCAL_PATH}")
 
 # Launch LabVIEW diff program
 "${DIFF_CMD}" "${REMOTE_WINPATH}" "${LOCAL_WINPATH}"
--- utils.sh
+++ utils.sh
@@ -1,4 +1,4 @@
-# "/c/Foo Bar/hoge fuga.vi" => "C:\Users\FOOBAR~1\HOGEFU~1.VI"
-function dospath () {
-  echo $(cygpath -d "$1")
+# "hoge fuga.vi" => "C:\Users\FOOBAR~1\HOGEFU~1.VI"
+function resolve_path () {
+  echo $(cygpath -d -a "$1")
 }
