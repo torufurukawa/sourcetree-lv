947af70ebfc5998bd455333ffc15de4c602f337a
Toru Furukawa <torufurukawa@gmail.com>
2016-11-30 20:36:31 +0900

    Fix LabVIEW path

--- src/merge.sh
+++ src/merge.sh
@@ -2,8 +2,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 source "${SCRIPT_DIR}/config.txt"
 source "${SCRIPT_DIR}/utils.sh"
 
-# "${LabViewShared}/LabVIEW Merge/LVMerge.exe" "${LabViewBin}" "${BASE}" "${REMOTE}" "${LOCAL}" "${MERGED}"
-
 # Clean paths
 BASE=$(resolve_path "$1")
 REMOTE=$(resolve_path "$2")
@@ -11,4 +9,4 @@ LOCAL=$(resolve_path "$3")
 MERGED=$(resolve_path "$4")
 
 # Launch LabVIEW merge program
-"${MERGE_CMD}" "${LabViewBin}" "${BASE}" "${REMOTE}" "${LOCAL}" "${MERGED}"
+"${MERGE_CMD}" "${LABVIEW_CMD}" "${BASE}" "${REMOTE}" "${LOCAL}" "${MERGED}"
