348177d9052cf7d937605a029e9d60324e8c9670
Toru Furukawa <torufurukawa@gmail.com>
2016-12-02 12:41:14 +0900

    Add reading local configuration

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,13 @@
 SCRIPT_DIR=${SCRIPT_DIR-"$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"}
 source "${SCRIPT_DIR}/config.txt"
 
+# read local config
+LOCAL_CONFIG="$(pwd)/lvdiffmerge.txt"
+if [ -e "${LOCAL_CONFIG}" ]
+then
+	source "${LOCAL_CONFIG}"
+fi
+
 # "hoge fuga.vi" => "C:\Users\FOOBAR~1\HOGEFU~1.VI"
 function resolve_path () {
   echo $(cygpath -d -a "$1")
